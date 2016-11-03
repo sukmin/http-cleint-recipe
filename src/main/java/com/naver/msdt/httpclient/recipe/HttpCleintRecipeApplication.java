21082a772a456e06b87efa77930114926ea34c7c
sukmin <killroad@gmail.com>
2016-11-03 23:24:22 +0900

    최초 커밋

---
+++ src/main/java/com/naver/msdt/httpclient/recipe/HttpCleintRecipeApplication.java
@@ -0,0 +1,12 @@
+package com.naver.msdt.httpclient.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpCleintRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpCleintRecipeApplication.class, args);
+	}
+}
