484535ca8d641254f7a8a3fde5a2983f6cb6d85d
권석민 <killroad@gmail.com>
2018-05-10 19:14:52 +0900

    put, delete 테스트 메소드 추가

--- src/main/java/com/naver/msdt/httpclient/recipe/web/SimpleTestController.java
+++ src/main/java/com/naver/msdt/httpclient/recipe/web/SimpleTestController.java
@@ -1,5 +1,7 @@
 package com.naver.msdt.httpclient.recipe.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,23 +9,39 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+
 import java.util.Map;
 
 /**
  * Created by john on 2016-11-04.
  */
 @RestController
-@RequestMapping(path="/simple")
+@RequestMapping(path = "/simple")
 public class SimpleTestController {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String get() {
-        return "ok";
-    }
+	private final static Logger LOGGER = LoggerFactory.getLogger(SimpleTestController.class);
 
-    @RequestMapping(method = RequestMethod.POST)
-    public Map<String, String[]> post(HttpServletRequest request) {
-        return request.getParameterMap();
-    }
+	@RequestMapping(method = RequestMethod.GET)
+	public String get() {
+		return "ok";
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public Map<String, String[]> post(HttpServletRequest request) {
+		LOGGER.info("this post");
+		return request.getParameterMap();
+	}
+
+	@RequestMapping(method = RequestMethod.PUT)
+	public Map<String, String[]> put(HttpServletRequest request) {
+		LOGGER.info("this put");
+		return request.getParameterMap();
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE)
+	public Map<String, String[]> delete(HttpServletRequest request) {
+		LOGGER.info("this delete");
+		return request.getParameterMap();
+	}
 
 }
