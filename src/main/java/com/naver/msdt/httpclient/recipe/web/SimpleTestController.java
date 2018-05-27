86cf76f98cc717019ce84a53407d816490ba1eca
권석민 <killroad@gmail.com>
2018-05-28 01:06:08 +0900

    테스트 컨트롤러 추가

--- src/main/java/com/naver/msdt/httpclient/recipe/web/SimpleTestController.java
+++ src/main/java/com/naver/msdt/httpclient/recipe/web/SimpleTestController.java
@@ -2,6 +2,8 @@ package com.naver.msdt.httpclient.recipe.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -42,6 +44,18 @@ public class SimpleTestController {
 	public Map<String, String[]> delete(HttpServletRequest request) {
 		LOGGER.info("this delete");
 		return request.getParameterMap();
+	}
+
+	@RequestMapping(path = "/empty", method = RequestMethod.POST)
+	public ResponseEntity<Void> emptyPost() {
+		LOGGER.info("this emptyPost");
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+	@RequestMapping(path = "/400", method = RequestMethod.GET)
+	public ResponseEntity<String> get400() {
+		LOGGER.info("this emptyPost");
+		return new ResponseEntity<>("400", HttpStatus.BAD_REQUEST);
 	}
 
 }
